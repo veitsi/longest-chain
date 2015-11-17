9951da40dc9281a4aa317b9b57a84206230ccd3d
xtfkpi <vitaliy@brightlocal.com>
2015-11-18 00:58:08 +0200

    it works

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 console.log('longest chain started');
 function chainLength(a){
-    return 0;
+    if (a.length>100) {
+        throw new Error("too long string");
+    }
+    var maxl=0;
+    var i= a.length-1;
+    var inseq=false;
+    var l=0;
+    while (i>-1){
+        if (a[i]==="1"){
+            if (!inseq){
+                inseq=true;
+                l=1;
+            }
+            else {l++}
+        }
+        else if (inseq){
+            inseq=false;
+            if (l>maxl) maxl=l;
+        }
+        i--;
+    }
+    if (l>maxl) maxl=l;
+    return maxl;
 }
--- main.test.js
+++ main.test.js
@@ -4,14 +4,31 @@ assert=require('assert');
 eval(fs.readFileSync('/home/jsdev/longest-chain/main.js')+'');
 
 suite('basic check for chainLength', function(){
+    test('should return 0 for dirty string', function(){
+        //assert.equal(chainLength("ghfghfghfghfdhfdhfdhfgh"),0);
+        //to check if error
+    });
     test('should return 0 for empty string', function(){
         assert.equal(chainLength(""),0);
     });
+
     test('should return 0 for 0 string', function(){
         assert.equal(chainLength("0"),0);
     });
+    test('should return 0 for "0000000000" string', function(){
+        assert.equal(chainLength("0000000000"),0);
+    });
     test('should return 1 for string with only 1', function(){
         assert.equal(chainLength("1"),1);
-    })
+    });
+    test('should return 5 for string "11111"', function(){
+        assert.equal(chainLength("11111"),5);
+    });
+    test('should return 3 for string "000111000"', function(){
+        assert.equal(chainLength("000111000"),3);
+    });
+    test('should return 4 for string "11101111000"', function(){
+        assert.equal(chainLength("110001111000"),4);
+    });
 
 });
